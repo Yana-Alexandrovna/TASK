ca35d7b35db579b9d85d534c72ef9e4819ac05f9
Iana <120241775+Yana-Alexandrovna@users.noreply.github.com>
2022-12-24 12:49:39 +0300

    добавили задание 10

--- TASK10/Program.cs
+++ TASK10/Program.cs
@@ -3,3 +3,20 @@
 //456 -> 5
 //782 -> 8
 //918 -> 1
+
+Console.WriteLine("Введите трехзначное число ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = a.ToString().Length;
+
+if (b != 3)
+{
+    Console.WriteLine("Ошибка ввода");
+}
+else
+{
+    
+int c = a/10;
+int d = c%10;
+Console.WriteLine("Вторая цифра " + d);
+
+}
