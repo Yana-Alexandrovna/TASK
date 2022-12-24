4f4cf1f460a5c4f02c6b4e40a4565ec56f0832df
Iana <120241775+Yana-Alexandrovna@users.noreply.github.com>
2022-12-24 14:34:56 +0300

    добавили задание 13

--- TASK13/Program.cs
+++ TASK13/Program.cs
@@ -3,3 +3,22 @@
 //645 -> 5
 //78 -> третьей цифры нет
 //32679 -> 6
+
+Console.WriteLine("Введите число ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = a.ToString().Length;
+int c = 1;
+if ( b < 3)
+{
+    Console.WriteLine("третьей цифры нет");
+}
+else 
+{
+ while (b > 3)
+ {
+  c = c * 10;
+  b = b - 1;
+ }
+int d = (a / c) % 10;
+Console.WriteLine("Третья цифра " + d);
+}
