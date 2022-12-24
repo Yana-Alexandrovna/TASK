6f722fcd5d08311c897ff5503af37b25bbe470e5
Iana <120241775+Yana-Alexandrovna@users.noreply.github.com>
2022-12-24 12:09:25 +0300

    Добавили задание #15

--- TASK15/Program.cs
+++ TASK15/Program.cs
@@ -2,3 +2,19 @@
 //6 -> да
 //7 -> да
 //1 -> нет
+
+
+
+Console.WriteLine("Введите номер ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if ( a == 1 ) Console.Write("Нет"); 
+else if ( a == 2 ) Console.Write("Нет"); 
+else if ( a == 3 ) Console.Write("Нет"); 
+else if ( a == 4 ) Console.Write("Нет"); 
+else if ( a == 5 ) Console.Write("Нет"); 
+else if ( a == 6 ) Console.Write("Да"); 
+else if ( a == 7 ) Console.Write("Да"); 
+else Console.Write("Ошибка"); 
+
+
