6987dd9c44ab38f7b8e932ca0cfc7c5ea7401e27
Iana <120241775+Yana-Alexandrovna@users.noreply.github.com>
2023-01-31 17:02:02 +0300

    43

--- TASK 42/Program.cs
+++ TASK 42/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+
+void GetBinaryView(int number)
+{
+if(number == 0) return;
+GetBinaryView(number/2);
+Console.Write(number%2);
+}
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+GetBinaryView(number);
--- TASK 43/Program.cs
+++ TASK 43/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+double ResaltY( double b, double k, double t)
+{
+    
+    double y = k * t + b;
+    return y;
+}
+
+Console.WriteLine("Введите b1 ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите k1 ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите b2 ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите k2 ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+if (k1 == k2 && b1 == b2)
+Console.WriteLine("Эти прямые лежат друг на друге"); 
+else if (k1 == k2 && b1 != b2)
+Console.WriteLine("Это параллельные прямые"); 
+else 
+{
+  double x = (b2 - b1) / (k1 - k2);  
+ 
+  Console.Write($" {x} , {ResaltY(b1, k1, x)}" );
+}  
