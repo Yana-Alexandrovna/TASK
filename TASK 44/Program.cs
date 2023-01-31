86b85496ac521d1813a3b51b312d6f91d7e094ef
Iana <120241775+Yana-Alexandrovna@users.noreply.github.com>
2023-01-31 17:11:42 +0300

    44 45

--- TASK 44/Program.cs
+++ TASK 44/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 44: Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+// Если N = 5 -> 0 1 1 2 3
+// Если N = 3 -> 0 1 1
+// Если N = 7 -> 0 1 1 2 3 5 8
+
+void PrintFib(int n)
+{
+Console.Write("0 1 ");
+int neighbour1 = 0;
+int neighbour2 = 1;
+int res = 0;
+for(int i = 0; i < n - 2; i++)
+{
+res = neighbour1 + neighbour2;
+Console.Write(res + " ");
+neighbour1 = neighbour2;
+neighbour2 = res;
+}
+}
+
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+PrintFib(N);
--- TASK 45/Program.cs
+++ TASK 45/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 45: Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования.
+
+
+
+void FillArrayWithRandom(int[] array)
+{
+for(int i = 0; i < array.Length; i++)
+{
+array[i] = new Random().Next(1, 10);
+}
+}
+
+int[] array1 = new int [5];
+FillArrayWithRandom(array1);
+int[] array2 = new int [5];
+Console.Write("Ваш случайный массив: ");
+Console.WriteLine(string.Join(", ", array1));
+
+for(int i = 0; i < array2.Length; i++)
+{
+array2[i] = array1[i];
+}
+Console.Write("Ваш скопированный массив: ");
+Console.WriteLine(string.Join(", ", array2));
+
