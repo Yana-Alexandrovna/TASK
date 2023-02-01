a4307e8332eeb32c10b32b3f7ee431625507f6f6
Iana <120241775+Yana-Alexandrovna@users.noreply.github.com>
2023-02-01 18:06:15 +0300

    до 52

--- TASK 52/Program.cs
+++ TASK 52/Program.cs
@@ -4,3 +4,38 @@
 //5 9 2 3
 //8 4 2 4
 //Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+void FillArrayWithRandom(int[,] matrix, int m, int n)
+{
+for(int i = 0; i < m; i++)
+{
+for(int j = 0; j < n; j++)
+{
+matrix[i,j] = new Random().Next(0, 11);
+Console.Write(matrix[i,j] + "\t");
+}
+Console.WriteLine();
+}
+Console.WriteLine();
+}
+
+void ArithmeticMean(int[,] matrix, int m, int n)
+{
+for(int j = 0; j < n; j++)
+{
+double Sum = 0;    
+for(int i = 0; i < m; i++) 
+{
+Sum += matrix[i,j];
+}
+Console.Write($"" + Sum/m + "   ");
+}
+}
+
+Console.WriteLine("Введите число строк:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число столбцов:");
+int n = Convert.ToInt32(Console.ReadLine());
+int [,] matrix = new int[m,n];
+FillArrayWithRandom(matrix, m, n);
+ArithmeticMean(matrix, m, n);
