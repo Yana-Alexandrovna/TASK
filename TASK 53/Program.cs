9b4076e7e7235516f55ef9cac0c3a2684dbbedee
Iana <120241775+Yana-Alexandrovna@users.noreply.github.com>
2023-02-03 19:33:48 +0300

    53,54

--- TASK 53/Program.cs
+++ TASK 53/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 53: Задайте двумерный массив.
+// Напишите программу, которая поменяет местами первую и последнюю строку массива.
+
+void FillArrayWithRandom(int[,] matrix)
+{
+for(int i = 0; i < matrix.GetLength(0); i++)
+{
+for(int j = 0; j < matrix.GetLength(1); j++)
+{
+matrix[i,j] = new Random().Next(0, 10);
+}
+}
+}
+
+void PrintArrayWithRandom(int[,] matrix)
+{
+for(int i = 0; i < matrix.GetLength(0); i++)
+{
+for(int j = 0; j < matrix.GetLength(1); j++)
+{
+Console.Write(matrix[i,j] + "\t");
+}
+Console.WriteLine();
+}
+Console.WriteLine();
+}
+
+Console.WriteLine("Введите число m:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число n:");
+int n = Convert.ToInt32(Console.ReadLine());
+int [,] matrix = new int[m,n];
+FillArrayWithRandom(matrix);
+PrintArrayWithRandom(matrix);
+
+int temp = 0;
+
+for(int i = 0; i < matrix.GetLength(1); i++)
+{
+temp = matrix[0,i];
+matrix[0, i] = matrix[matrix.GetLength(0) - 1, i];
+matrix[matrix.GetLength(0) - 1, i] = temp;
+}
+Console.WriteLine();
+PrintArrayWithRandom(matrix);
--- TASK 54/Program.cs
+++ TASK 54/Program.cs
@@ -1,2 +1,58 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//В итоге получается вот такой массив:
+//7 4 2 1
+//9 5 3 2
+//8 4 4 2
+
+void FillArrayWithRandom(int[,] matrix)
+{
+for(int i = 0; i < matrix.GetLength(0); i++)
+{
+for(int j = 0; j < matrix.GetLength(1); j++)
+{
+matrix[i,j] = new Random().Next(0, 10);
+}
+}
+}
+
+void PrintArrayWithRandom(int[,] matrix)
+{
+for(int i = 0; i < matrix.GetLength(0); i++)
+{
+for(int j = 0; j < matrix.GetLength(1); j++)
+{
+Console.Write(matrix[i,j] + "\t");
+}
+Console.WriteLine();
+}
+Console.WriteLine();
+}
+ 
+Console.WriteLine("Введите число строк:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число столбцов:");
+int n = Convert.ToInt32(Console.ReadLine());
+int [,] matrix = new int[m,n];
+FillArrayWithRandom(matrix);
+PrintArrayWithRandom(matrix);
+for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(1) - 1; k++)
+            {
+                if (matrix[i, k] < matrix[i, k + 1])
+                {
+                    int max = matrix[i, k + 1];
+                    matrix[i, k + 1] = matrix[i, k];
+                    matrix[i, k] = max;
+                }
+            }
+        }
+    }
+Console.WriteLine();
+PrintArrayWithRandom(matrix);
