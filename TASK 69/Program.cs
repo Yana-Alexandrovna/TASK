df09190561ac3d54fe3bf1aa9c9807d8a6c625fe
Iana <120241775+Yana-Alexandrovna@users.noreply.github.com>
2023-02-07 19:32:54 +0300

    последние

---
+++ TASK 69/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 69 напишите программу, которая на вход принимает два числа А и В, и возводит число А в целую степень В с помощью рекурсии
+// А = 3; B = 5 -> 243
+// А = 2; B = 3 -> 8
+
+int Exp(int n, int m)
+{
+    int exp = 1;
+    if(m == 0)
+    return exp;
+    else return exp = n*Exp(n, m-1);
+}
+
+
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int exp = Exp(n,m);
+Console.WriteLine(exp);
